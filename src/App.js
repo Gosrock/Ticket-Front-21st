febfccbfea55f1083990aba433676d8be0153302
unknown <nohtak1213@gmail.com>
2022-01-09 21:19:51 +0900

    #8 나는왜됨

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>고스락 gn휘휘휘ldidididi야야야야론트</p>
+        <p>이찬진 파이팅 한규진 파이팅ㅇ 김원진 파이팅 론트</p>
       </header>
     </div>
   );

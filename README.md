e447d8183877aab374badac0e62f5ae4a6082e8e
9yujin <55226431+9yujin@users.noreply.github.com>
2022-01-09 21:31:17 +0900

    conflict 해결, 한줄 추가함 (#28)
    * #7
    
    * 한줄 추가
    
    Co-authored-by: Chan Jin <water0641@naver.com>

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 ### 커밋 이모지
 
-
-- :rocket: `:rocket:` : 기능 추가
-- :bug: `:bug:` : 버그
-- :books: `:books:` : 문서 작업
-
-- :hammer: `:hammer:` : 리팩토링 ㅎㅎ
-
-
-- :hankey: `:hankey:` : 악취가나는 코드 ( 리팩토링 필요 )
+-   :rocket: `:rocket:` : 기능 추가
+-   :bug: `:bug:` : 버그
+-   :books: `:books:` : 문서 작업
+-   :hammer: `:hammer:` : 리팩토링 ㅎㅎ
+-   :hankey: `:hankey:` : 악취가나는 코드 ( 리팩토링 필요 )
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>고스락 이찬진 짱짱</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>고스락 이찬진 짱짱</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;

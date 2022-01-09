8f5b13be6b2cdba83acbe24c11759c1d91b41fbc
unknown <nohtak1213@gmail.com>
2022-01-09 21:10:12 +0900

    #7 pull dev한거

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
-        <p>고스락 dsadsasdasdada휘론트</p>
-=======
-        <p>고스락ssss 찬진짱</p>
->>>>>>> 562f37a550e7d96bc6748038b20b207ff6f1b1d9
+        <p>고스락 gnl론트</p>
       </header>
     </div>
   );

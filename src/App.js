8173d4421160d0bf5e9688fea427eb66d3f73cd2
이찬진 <water0641@naver.com>
2022-01-09 20:37:33 +0900

    switich

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 function App() {
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>고스락 짱 한규진 짱</p>
-            </header>
-        </div>
-    );
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>고스락 짱 한규진 짱</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;

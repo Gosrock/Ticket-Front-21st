0a1dcaf221d0e2bf7e6b6152fc451814e1d64ee7
한규진 <david0218@naver.com>
2022-01-09 21:33:48 +0900

    한줄 추가 진짜

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <p>고스락 이찬진 짱짱</p>
+                <p>한줄 추가</p>
             </header>
         </div>
     );

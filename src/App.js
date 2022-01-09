264a42ce2a5a5b076b2a9fa377559c713c7dd7e5
이찬진 <water0641@naver.com>
2022-01-09 20:57:59 +0900

    conflict

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>고스락 기타 이찬진짱짱</p>
+        <p>고스락 check 이찬진짱짱</p>
       </header>
     </div>
   );

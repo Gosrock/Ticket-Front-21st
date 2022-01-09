2d347334f475de94b40840e88f7f144befe79f4e
이찬진 <water0641@naver.com>
2022-01-10 01:15:34 +0900

    rebase

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         <p>고스락 이찬진 짱짱</p>
         <p>되나? 되긴ㄴ뭐가되냐</p>
         <p>zzzzzzz</p>
-=======
-        <p>고스락 프론트</p>
->>>>>>> a65f7de (:hammer: #2 필요없는 파일 삭제 , 디팬던시 추가)
       </header>
     </div>
   );

3e5919407b8ef5cd3e17d9eb5b4bc858f4bed13d
이찬진 <water0641@naver.com>
2022-01-09 20:46:29 +0900

    push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>고스락 이찬진짱</p>
+        <p>고스락 기타 이찬진짱짱</p>
       </header>
     </div>
   );
-
 }
 
 export default App;

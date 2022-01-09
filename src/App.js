185af4456c170fdf9ee64ece463463c9cc9ea061
Chan Jin <water0641@naver.com>
2022-01-10 01:21:10 +0900

    Chanjin (#33)
    * rebase
    
    * rebase
    
    * rebase
    
    * 브랜치 없앤 이후
    
    * 리베이스 해봄

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>고스락 이찬진 짱짱</p>
+        <p>고스 짱짱</p>
         <p>되나? 되긴ㄴ뭐가되냐</p>
         <p>zzzzzzz</p>
       </header>

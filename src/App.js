7cc54864d1d3f4bb5661ca5837e7a770c692db40
한규진 <david0218@naver.com>
2022-01-09 20:18:39 +0900

    #7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>고스락 프론트</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>고스락 짱 한규진 짱</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;

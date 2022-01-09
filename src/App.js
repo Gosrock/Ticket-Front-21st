bfd3407e03a280a3a75cc3eca22a893814f1ad11
이찬진 <water0641@naver.com>
2022-01-09 21:22:18 +0900

    pushpush

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>고스락 이찬진 짱짱</p>
+        <p>고스락 이찬진 WkdWkdWkd</p>
       </header>
     </div>
   );

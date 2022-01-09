6696c7f5a719bfabb127989afa7e2c6b322b4463
Chan Jin <water0641@naver.com>
2022-01-10 01:29:06 +0900

    Chanjin (#34)
    * rebase
    
    * rebase
    
    * rebase
    
    * 브랜치 없앤 이후
    
    * 리베이스 해봄
    
    * 기능추가
    
    * 이상하게 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
       <header className="App-header">
         <p>고스 짱짱</p>
         <p>되나? 되긴ㄴ뭐가되냐</p>
-        <p>zzzzzzz</p>
+        <p>줄 다시 고침</p>
+        <p>줄 추가중입니당,</p>
       </header>
     </div>
   );

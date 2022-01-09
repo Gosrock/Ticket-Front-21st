34e40319910542ac6343cc8757b5a646a5902248
한규진 <david0218@naver.com>
2022-01-09 21:19:36 +0900

    한줄 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <p>고스락 gn휘휘휘ldidididi야야야야론트</p>
+                <p>한줄 추가</p>
             </header>
         </div>
     );

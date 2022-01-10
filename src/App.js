4fdd830ec1ddb8e603c2042ecfd4f3cbe9480486
Chan Jin <water0641@naver.com>
2022-01-10 16:47:33 +0900

    머지 (#38)
    * rebase
    
    * rebase
    
    * rebase
    
    * 브랜치 없앤 이후
    
    * 리베이스 해봄
    
    * 기능추가
    
    * 이상하게 수정
    
    * 프리티어 테스트
    
    * hsuky...
    
    * ...
    
    * lint?
    
    * lint
    
    * 프리티어 옵션추가?
    
    * wnf
    
    * 작업 수정

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>고스락 이찬진 짱짱</p>
-        <p>되나?</p>
-        <p>여기 고침</p>
+        <p>고스 짱짱</p>
+        <p>되나? 되긴ㄴ뭐가되냐</p>
+        <p>줄 다시 고침</p>
+        <p>줄 추가중입니당,</p>
+        asdfdl이제 자동으로 고침 수정수정
       </header>
     </div>
   );

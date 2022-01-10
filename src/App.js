fc4457666f6eaabe0f55c79cceba42d9d1db8c20
한규진 <david0218@naver.com>
2022-01-10 17:14:02 +0900

    머지용  커밋

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>고스 짱짱</p>
-        <p>git add. -> git commit -> git pull origin dev</p>
+        <p>git add . >> git commit >> git pull origin dev</p>
         <p>그 다음에 푸쉬합시다</p>
       </header>
     </div>

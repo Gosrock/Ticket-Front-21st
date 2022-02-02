2ef8ed0ddde96a6936e7720d2b29b7684d72b8bd
한규진 <david0218@naver.com>
2022-02-03 00:41:54 +0900

    모달 예시 구현

--- src/components/ListProcess/TicketListPage/TicketListPage.js
+++ src/components/ListProcess/TicketListPage/TicketListPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import moment from 'moment';
 import axios from 'axios';
 import {
   TicketContainer,
@@ -11,43 +12,57 @@ import {
   ProcessTitle,
   ProcessDescription,
   TicketList,
-  StateIcon
+  StateIcon,
+  Modal
 } from 'gosrock-storybook';
 import './TicketListPage.css';
 import history from '../../../history';
 import { useSelector } from 'react-redux';
 
-const testData = [
-  {
-    _id: '61e0453bb063b6962b939cdc',
-    status: 'pending-deposit',
-    phoneNumber: '01094768640',
-    ticketNumber: 2,
-    adminTicket: false,
-    accountName: '이찬진',
-    createdAt: '2022-01-13T15:28:59.133Z',
-    updatedAt: '2022-01-23T15:28:19.557Z',
-    __v: 0
-  }
-];
-
 function TicketListPage({ ...props }) {
   const { authenticated, phoneNumber, userAccessToken } = useSelector(
     state => state.auth
   );
-  const [tickets, setTickets] = useState();
-  const bodyBox = useRef();
   const bottomLabel = `${phoneNumber.replace(
     /^(\d{2,3})(\d{3,4})(\d{4})$/,
     `$1-$2-$3`
   )} 님!`;
 
+  const [tickets, setTickets] = useState();
+  const bodyBox = useRef();
+  const modalRef = useRef();
+
+  const handleResize = () => {
+    console.log(bodyBox.current.parentNode.clientHeight);
+    console.log(bodyBox.current.parentNode);
+    document.documentElement.style.setProperty(
+      '--listHeight',
+      `${bodyBox.current.parentNode.clientHeight}px`
+    );
+    const [container] = document.getElementsByClassName('Ticket-Container');
+    document.documentElement.style.setProperty(
+      '--containerHeight',
+      `${container.clientHeight}px`
+    );
+    document.documentElement.style.setProperty(
+      '--containerWidth',
+      `${container.clientWidth}px`
+    );
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      // cleanup
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const getMyTickets = async () => {
     try {
-      // axios.defaults.headers.common.Authorization = `Bearer ${userAccessToken}`;
-      // const response = await axios.get(`/tickets?phoneNumber=${phoneNumber}`);
-      // setTickets(response.data.data);
-      setTickets(testData);
+      axios.defaults.headers.common.Authorization = `Bearer ${userAccessToken}`;
+      const response = await axios.get(`/tickets?phoneNumber=${phoneNumber}`);
+      setTickets(response.data.data);
     } catch (e) {
       console.log(e.response.data);
     }
@@ -60,16 +75,7 @@ function TicketListPage({ ...props }) {
   }, [phoneNumber]);
 
   useEffect(() => {
-    console.log(tickets);
-  }, [tickets]);
-
-  useEffect(() => {
-    console.log(bodyBox.current.parentNode.clientHeight);
-    console.log(bodyBox.current.parentNode);
-    document.documentElement.style.setProperty(
-      '--height',
-      `${bodyBox.current.parentNode.clientHeight}px`
-    );
+    handleResize();
   }, []);
 
   /* const ViewTicketHandler = () => {
@@ -100,10 +106,10 @@ function TicketListPage({ ...props }) {
               <span
                 className="show-account"
                 onClick={() => {
-                  console.log('입금계좌보기');
+                  modalRef.current.classList.remove('hidden');
                 }}
               >
-                입금 계좌보기
+                입금계좌 보기
               </span>
             </p>
           </TicketBodyHeader>
@@ -139,7 +145,7 @@ function TicketListPage({ ...props }) {
                       <TicketList
                         key={v._id}
                         performdate="22.03.10"
-                        bookdate="22.02.18"
+                        bookdate={moment(v.createdAt).format('YY.MM.DD')}
                         StateIcon={state}
                         onClick={() => {
                           console.log('click');
@@ -148,6 +154,17 @@ function TicketListPage({ ...props }) {
                       />
                     );
                   })}
+              </div>
+            </div>
+            <div className="modal hidden" ref={modalRef}>
+              <div
+                className="modal-overlay"
+                onClick={() => {
+                  modalRef.current.classList.add('hidden');
+                }}
+              ></div>
+              <div className="modal-content">
+                <Modal />
               </div>
             </div>
           </TicketBody>

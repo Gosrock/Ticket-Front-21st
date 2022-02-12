2be3a6701280d05d9bbb514af515f1f0412adc27
이찬진 <water0641@naver.com>
2022-02-13 01:28:38 +0900

    #120 티켓 예매 신앱생 & 재학생

---
+++ src/components/TicketingProcess/TicketingDepositPage/TicketingOBDeposit.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  GoFrontButton,
+  TicketBodyHeader,
+  TicketBody,
+  TicketBottom,
+  ProcessTitle,
+  ProcessDescription,
+  InputForm,
+  Modal
+} from 'gosrock-storybook';
+// 재학생 졸업생용
+function TicketingOBDeposit({
+  accountName,
+  accountNameInputHandler,
+  modalRef,
+  kakaoClickButtonHandler,
+  purchaseButtonHandler,
+  frontButtonHandler
+}) {
+  return (
+    <>
+      <TicketBodyHeader>
+        <ProcessTitle topLabel="입금자명을" bottomLabel="입력해주세요." />
+        <ProcessDescription topLabel="이름은 4자 이내로 입력해주세요." />
+      </TicketBodyHeader>
+      <TicketBody>
+        <div className="input-form">
+          <InputForm
+            value={accountName}
+            onChange={accountNameInputHandler}
+            page="name"
+            ticketCount={1}
+          />
+        </div>
+        <div className="modal hidden" ref={modalRef}>
+          <div
+            className="modal-overlay"
+            onClick={() => {
+              modalRef.current.classList.add('hidden');
+            }}
+          ></div>
+          <div className="modal-content">
+            <Modal
+              onClickKakao={kakaoClickButtonHandler}
+              onClickPurchased={purchaseButtonHandler}
+            />
+          </div>
+        </div>
+      </TicketBody>
+      <TicketBottom>
+        <GoFrontButton
+          onClick={frontButtonHandler}
+          label="티켓 발급하기"
+        ></GoFrontButton>
+      </TicketBottom>
+    </>
+  );
+}
+
+export default TicketingOBDeposit;
